0331f421914b611817a98ce65b07ada036a37cd9
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2025-06-22 00:42:00 +0530

    Update r_optimized.py

--- r_optimized.py
+++ r_optimized.py
@@ -70,7 +70,7 @@ def filter_persona_by_traits(persona_content, selected_traits):
 
 def call_gemini_local(query, previous_conversation, gender, username, botname, bot_prompt, llm_api_key_string, language):
     try:
-        language_instruction = f"Respond in {language} language in 2 or 3 lines only, unless longer answers are expected." if language != "English" else "English"
+        language_instruction = f"Respond in {language} language in 2 or 3 lines only, unless longer answers are expected." if language != "English" else "English" 
         full_prompt = (
             f"{bot_prompt}\n"
             f"{language_instruction}\n"
