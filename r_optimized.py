fdb8df01a996d6037c1dbc18f13002effecc8872
vedantvbarve <156084437+vedantvbarve@users.noreply.github.com>
2025-07-14 23:00:19 +0400

    Update r_optimized.py

--- r_optimized.py
+++ r_optimized.py
@@ -6,7 +6,7 @@ import glob
 import requests
 
 # ====== CONFIGURATION ======
-FASTAPI_URL = "https://emirati-personas-streamlit-476878255400.me-central1.run.app"  # <-- CHANGE THIS
+FASTAPI_URL = "https://emirati-personas-api-2l5aaarlka-ww.a.run.app"  # <-- CHANGE THIS
 
 PERSONAS_FOLDER = "Personas"
 QUESTIONS_FOLDER = "Questions"
